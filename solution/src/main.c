5c6c922f1f4d47d4b8810b8fe2838421addfd632
deadxraver <409853@edu.itmo.ru>
2024-12-04 02:46:00 +0300

    linter checks 2

--- solution/src/main.c
+++ solution/src/main.c
@@ -1,7 +1,8 @@
 #include "bmp.h"
+#include "transform.h"
+
 #include <stdio.h>
 #include <string.h>
-#include "transform.h"
 
 int main( int argc, char** argv ) {
   if ( argc == 2 && ( strcmp( argv[1], "help" ) == 0 || strcmp( argv[1], "-help" ) == 0 || strcmp( argv[1], "--help" ) == 0 || strcmp( argv[1], "-h" ) == 0 ) ){
